f6d5ade005c79629adb2916375af88ee87de551c
FifionRibana <80655831+FifionRibana@users.noreply.github.com>
2026-03-13 18:59:38 +0100

    FEAT : Disable map entries when views or panels are displayed [#42]

--- crates/client/src/camera/plugin.rs
+++ crates/client/src/camera/plugin.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 
 use crate::states::AppState;
+use crate::states::GameView;
 
 use super::components;
 use super::input;
@@ -10,10 +11,12 @@ pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .init_resource::<resources::CameraSettings>()
+        app.init_resource::<resources::CameraSettings>()
             .add_systems(Startup, components::setup_camera)
-            .add_systems(Update, (input::camera_movement, input::camera_zoom))
+            .add_systems(
+                Update,
+                (input::camera_movement, input::camera_zoom).run_if(in_state(GameView::Map)),
+            )
             .add_systems(OnEnter(AppState::InGame), components::center_camera_on_lord);
     }
 }
